48e89a7e160b2f329a054e1b60e7368b05a8d806
unknown <info@schedulize.co.ke>
2023-09-30 15:35:39 +0300

    Courses on home page

---
+++ resources/views/frontend/includes/desktop/banner.blade.php
@@ -0,0 +1,155 @@
+<div class="sec-banner bg0">
+    <div class="flex-w flex-c-m">
+        <div class="size-202 m-lr-auto respon4">
+            <!-- Block1 -->
+            <div class="block1 wrap-pic-w">
+                <img src="{{ asset('storage/banner/banner-04.jpg') }}" alt="IMG-BANNER">
+
+                <a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+                    <div class="block1-txt-child1 flex-col-l">
+                        <span class="block1-name ltext-102 trans-04 p-b-8" style="color: #fff;">
+                            Board Certifications
+                        </span>
+
+                        <span class="block1-info stext-102 trans-04" style="color: #fff;">
+                            The Best Industry has to offer
+                        </span>
+                    </div>
+
+                    <div class="block1-txt-child2 p-b-4 trans-05">
+                        <div class="block1-link stext-101 cl0 trans-09">
+                            Discover Now
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="size-202 m-lr-auto respon4">
+            <!-- Block1 -->
+            <div class="block1 wrap-pic-w">
+                <img src="{{ asset('storage/banner/banner-06.jpg') }}" alt="IMG-BANNER">
+
+                <a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+                    <div class="block1-txt-child1 flex-col-l">
+                        <span class="block1-name ltext-102 trans-04 p-b-8" style="color: #fff;">
+                            Training Programs
+                        </span>
+
+                        <span class="block1-info stext-102 trans-04" style="color: #fff;">
+                            Up to 40% off on selected programs
+                        </span>
+                    </div>
+
+                    <div class="block1-txt-child2 p-b-4 trans-05">
+                        <div class="block1-link stext-101 cl0 trans-09">
+                            Explore programs
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="size-202 m-lr-auto respon4">
+            <!-- Block1 -->
+            <div class="block1 wrap-pic-w">
+                <img src="{{ asset('storage/banner/banner-05.jpg') }}" alt="IMG-BANNER">
+
+                <a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+                    <div class="block1-txt-child1 flex-col-l">
+                        <span class="block1-name ltext-102 trans-04 p-b-8" style="color: #fff;">
+                            Examination
+                        </span>
+
+                        <span class="block1-info stext-102 trans-04" style="color: #fff;">
+                            Sit For a Board Exam Today
+                        </span>
+                    </div>
+
+                    <div class="block1-txt-child2 p-b-4 trans-05">
+                        <div class="block1-link stext-101 cl0 trans-09">
+                            Learn More
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+
+        <div class="size-202 m-lr-auto respon4">
+            <!-- Block1 -->
+            <div class="block1 wrap-pic-w">
+                <img src="{{ asset('storage/banner/banner-04.jpg') }}" alt="IMG-BANNER">
+
+                <a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+                    <div class="block1-txt-child1 flex-col-l">
+                        <span class="block1-name ltext-102 trans-04 p-b-8" style="color: #fff;">
+                            Board Certifications
+                        </span>
+
+                        <span class="block1-info stext-102 trans-04" style="color: #fff;">
+                            The Best Industry has to offer
+                        </span>
+                    </div>
+
+                    <div class="block1-txt-child2 p-b-4 trans-05">
+                        <div class="block1-link stext-101 cl0 trans-09">
+                            Discover Now
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="size-202 m-lr-auto respon4">
+            <!-- Block1 -->
+            <div class="block1 wrap-pic-w">
+                <img src="{{ asset('storage/banner/banner-06.jpg') }}" alt="IMG-BANNER">
+
+                <a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+                    <div class="block1-txt-child1 flex-col-l">
+                        <span class="block1-name ltext-102 trans-04 p-b-8" style="color: #fff;">
+                            Training Programs
+                        </span>
+
+                        <span class="block1-info stext-102 trans-04" style="color: #fff;">
+                            Up to 40% off on selected programs
+                        </span>
+                    </div>
+
+                    <div class="block1-txt-child2 p-b-4 trans-05">
+                        <div class="block1-link stext-101 cl0 trans-09">
+                            Explore programs
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="size-202 m-lr-auto respon4">
+            <!-- Block1 -->
+            <div class="block1 wrap-pic-w">
+                <img src="{{ asset('storage/banner/banner-05.jpg') }}" alt="IMG-BANNER">
+
+                <a href="product.html"
+                    class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+                    <div class="block1-txt-child1 flex-col-l">
+                        <span class="block1-name ltext-102 trans-04 p-b-8" style="color: #fff;">
+                            Examination
+                        </span>
+
+                        <span class="block1-info stext-102 trans-04" style="color: #fff;">
+                            Sit For a Board Exam Today
+                        </span>
+                    </div>
+
+                    <div class="block1-txt-child2 p-b-4 trans-05">
+                        <div class="block1-link stext-101 cl0 trans-09">
+                            Learn More
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/frontend/includes/desktop/popular-courses.blade.php
+++ resources/views/frontend/includes/desktop/popular-courses.blade.php
@@ -12,7 +12,12 @@
                 <!-- Nav tabs -->
                 <ul class="nav nav-tabs" role="tablist">
                     <li class="nav-item p-b-10">
-                        <a class="nav-link active" data-toggle="tab" href="#featured" role="tab">
+                        <a class="nav-link active" data-toggle="tab" href="#featuredTab" role="tab">
+                            @lang('labels.frontend.layouts.partials.browse_featured_course')
+                        </a>
+                    </li>
+                    <li class="nav-item p-b-10">
+                        <a class="nav-link" data-toggle="tab" href="#popular" role="tab">
                             @lang('labels.frontend.layouts.partials.popular_courses')
                         </a>
                     </li>
@@ -20,7 +25,60 @@
 
                 <!-- Tab panes -->
                 <div class="tab-content p-t-50">
-                    <div class="tab-pane fade show active" id="featured" role="tabpanel">
+                    <div class="tab-pane fade show active" id="featuredTab" role="tabpanel">
+                        @if (count($featured_courses) > 0)
+                            <!-- Slide2 -->
+                            <div class="wrap-slick2">
+                                <div class="slick2">
+                                    @foreach ($featured_courses as $item)
+                                        <div class="item-slick2 p-l-15 p-r-15 p-t-15 p-b-15">
+                                            <!-- Block2 -->
+                                            <div class="block2">
+                                                <div class="block2-pic hov-img0">
+                                                    <img src="{{ asset('storage/uploads/' . $item->course_image) }}"
+                                                        alt="{{ $item->title }}">
+
+                                                    <a href="#"
+                                                        class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
+                                                        Quick View
+                                                    </a>
+                                                </div>
+                                                <!--/.block2-pic -->
+                                                <div class="block2-txt flex-w flex-t p-t-14">
+                                                    <div class="block2-txt-child1 flex-col-l ">
+                                                        <a href="product-detail.html"
+                                                            class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                                                            {{ $item->title }}
+                                                        </a>
+
+                                                        <span class="stext-105 cl3">
+                                                            {{ $appCurrency['symbol'] . ' ' . $item->price }}
+                                                        </span>
+                                                    </div>
+
+                                                    <div class="block2-txt-child2 flex-r p-t-3">
+                                                        <a href="#"
+                                                            class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+                                                            <img class="icon-heart1 dis-block trans-04"
+                                                                src="{{ asset('storage/images/icons/icon-heart-01.png') }}"
+                                                                alt="ICON">
+                                                            <img class="icon-heart2 dis-block trans-04 ab-t-l"
+                                                                src="{{ asset('storage/images/icons/icon-heart-02.png') }}"
+                                                                alt="ICON">
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!--/.item-slick2 -->
+                                    @endforeach
+                                </div>
+                                <!--/.slick2 -->
+                            </div>
+                            <!--/.wrap-slick2 -->
+                        @endif
+                    </div>
+                    <div class="tab-pane fade" id="popular" role="tabpanel">
                         <!-- Slide2 -->
                         <div class="wrap-slick2">
                             <div class="slick2">
@@ -29,7 +87,7 @@
                                         <!-- Block2 -->
                                         <div class="block2">
                                             <div class="block2-pic hov-img0">
-                                                <img src="{{ asset('storage/uploads/'.$item->course_image) }}"
+                                                <img src="{{ asset('storage/uploads/' . $item->course_image) }}"
                                                     alt="IMG-PRODUCT">
 
                                                 <a href="#"
@@ -42,7 +100,7 @@
                                                 <div class="block2-txt-child1 flex-col-l ">
                                                     <a href="product-detail.html"
                                                         class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                                                        {{$item->title}}
+                                                        {{ $item->title }}
                                                     </a>
 
                                                     <span class="stext-105 cl3">
