ba460861cc0eeb62edbada5fc815f6de594a477b
unknown <info@schedulize.co.ke>
2023-12-19 10:52:22 +0300

    Partners page

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -9,7 +9,8 @@
         .content img {
             margin: 10px;
         }
-        .about-page-section ul{
+
+        .about-page-section ul {
             padding-left: 20px;
             font-size: 20px;
             color: #333333;
@@ -18,11 +19,12 @@
         }
     </style>
 @endpush
-    
+
 @section('content')
 
     <!-- Title page -->
-    <section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('{{ asset('storage/bg/bg-04.jpg') }}');"></section>
+    <section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('{{ asset('storage/bg/bg-04.jpg') }}');">
+    </section>
 
     <!-- Content page -->
     <section class="bg0 p-t-75 p-b-120">
@@ -30,7 +32,7 @@
             <div class="row p-b-148">
                 @if ($page->image != null)
                     <div class="col-md-7 col-lg-8">
-                        <div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
+                        <div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md about-content">
                             <h3 class="mtext-111 cl2 p-b-16 txt-center">
                                 {{ $page->title }}
                             </h3>
@@ -41,12 +43,11 @@
                     </div>
 
                     <div class="col-11 col-md-5 col-lg-4 m-lr-auto">
-                        <div class="how-bor1 ">
+                        <div class="">
                             <div class="hov-img0">
                                 <img src="{{ asset('storage/uploads/' . $page->image) }}" alt="{{ $page->title }}">
                             </div>
                         </div>
-
                     </div>
                 @else
                     <div class="col-md-12 col-lg-12">
@@ -62,6 +63,53 @@
 
             </div>
             <!--/.row -->
+
+            @if ($page->slug == 'our-partners')
+                <div class="row">
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-01.png') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-02.png') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-03.jpeg') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-04.jpeg') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-05.png') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-06.png') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-07.png') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-08.png') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-09.png') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-10.png') }}" alt="" class="img-circle">
+                    </div> 
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-11.png') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-12.png') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-13.png') }}" alt="" class="img-circle">
+                    </div>
+                    <div class="col-md-2 partners-img">
+                        <img src="{{ asset('storage/partners/partner-14.png') }}" alt="" class="img-circle">
+                    </div>
+                </div>
+            @endif
         </div>
         <!--/.container -->
     </section>
